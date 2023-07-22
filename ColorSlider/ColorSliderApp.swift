3031759fed05620dddcde7093f17aba1c8f1ddab
NetRunner <galatskiy@gmail.com>
2023-07-22 18:42:01 +0300

    Initial Commit

---
+++ ColorSlider/ColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderApp.swift
+//  ColorSlider
+//
+//  Created by Dmitrii Galatskii on 22.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
